6223fd82bf0ca039233a8f122aac8f37ca7283f4
archaeophoenix <aleppo.coy@gmail.com>
2016-02-01 22:54:57 +0700

    Update Database.php

--- libs/Database.php
+++ libs/Database.php
@@ -39,6 +39,7 @@ class Database extends PDO{
                 $exe->bindValue(":$key", $value);
             }
             $exe->execute();
+            return $this->lastInsertId();
         }
     }
 
